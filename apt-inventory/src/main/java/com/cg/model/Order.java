80a4528c091b1226a928dfce5a7541647e283dfb
TruongVanPhon <truongvanphon020297@gmail.com>
2022-09-12 11:28:33 +0700

    update table order

--- apt-inventory/src/main/java/com/cg/model/Order.java
+++ apt-inventory/src/main/java/com/cg/model/Order.java
@@ -11,6 +11,7 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -33,6 +34,14 @@ public class Order {
 
     @Column(name = "quantity_total")
     private int quantityTotal;
+
+    private String description;
+
+    @Column(name = "inventory_delivery_date")
+    private LocalDate inventoryDeliveryDate;
+
+    @Column(name = "delivery_date")
+    private LocalDate deliveryDate;
 
     @OneToOne
     @JoinColumn(name = "location_region_delivery_id", nullable = false)
